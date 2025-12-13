5f7ae8f8752a2f66b7a0b7f2367c709778c7ac4f
willy willy <medicalawn@gmail.com>
2025-12-13 17:05:12 +0700

    feat: initialize Vue Router with login and merchant home routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../views/LoginPage.vue'
 import MerchantHome from '../views/MerchantHome.vue'
 
 const routes = [
-    {path: '/', redirect: '/login'},
-    {path: '/login', name: 'Login', component: LoginPage},
-    {path: '/merchants/:id', name: 'MerchantHome', component: MerchantHome, props: true, meta: { requiresAuth: true } },
+    { path: '/', redirect: '/login' },
+    { path: '/login', name: 'LoginPage', component: LoginPage },
+    { path: '/merchants/:id', name: 'MerchantHome', component: MerchantHome, props: true, meta: { requiresAuth: true } },
     { path: '/merchants', name: 'Merchants', component: MerchantHome } // fallback if you want a list route
 ]
 
