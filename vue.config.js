bf0a1ae8b9afd8fb62dabf3deb63b7fe105083ea
willy willy <medicalawn@gmail.com>
2025-12-13 19:20:40 +0700

    feat: configure dev server to allow all hosts and specify client WebSocket URL.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: 'auto://0.0.0.0:0/ws',
+    },
     proxy: {
       '/api': {
         target: 'http://app.netbird.cloud:8080',
